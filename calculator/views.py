aa41e9d6b4c8470486d1db07756cfaf418bc0f47
Neto test <zhuravlv-vlad@bk.ru>
2024-08-08 01:00:58 +0400

    first commit

---
+++ calculator/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+def get_omlet(request):
+    quan = request.GET.get('quantity')
+    a =0
+    if quan != None:
+        quan = int(quan)
+        rec = DATA["omlet"]
+        context = {
+            'recipe':{
+                'яйца': f"количество{in1}"
+
+            }
+        }
+
+    return HttpResponse(f"hello {a['яйца, шт']}")
+
+
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
